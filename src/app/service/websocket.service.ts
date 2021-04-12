e072e41f86058fdf8d02599103b6df63207e3298
SARVESH YADAV <sarveshyadav@SARVESHs-MacBook-Air.local>
2021-04-12 11:58:09 +0530

    loading

--- src/app/service/websocket.service.ts
+++ src/app/service/websocket.service.ts
@@ -1,6 +1,6 @@
 import {Injectable}   from '@angular/core';
 
-const WEBSOCKET_URL = 'ws://chat-deck.herokuapp.com/websocket';
+const WEBSOCKET_URL = 'wss://chat-deck.herokuapp.com/websocket';
 
 @Injectable()
 export class WebSocketService {
